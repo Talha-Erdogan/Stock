f9ad053e7de34490d057159eb56c370e6a6f9f50
talha erdoğan <talha_erd_91@hotmail.com>
2019-08-19 09:34:13 +0300

    Müşteri listeleme sayfası eklendi

--- Stock.UI/Controllers/CustomerController.cs
+++ Stock.UI/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Stock.UI.Helper;
+﻿using Stock.Business;
+using Stock.UI.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,36 @@ namespace Stock.UI.Controllers
     [AppAuthorize]
     public class CustomerController : Controller
     {
+        #region Private Member
+        private readonly CustomerService customerService;
+        #endregion
+        #region Constructor
+        public CustomerController()
+        {
+            customerService = new CustomerService();
+        }
+        #endregion
         // GET: Admin
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult List()
+        {
+            return this.Json(
+            new
+            {
+                Result = (from cus in customerService.GetAll()
+                          select new
+                          {
+                              Id = cus.Id,
+                              Debt = cus.Debt,
+                              Name = cus.Name,
+                              Surname = cus.Surname,
+                              Phone = cus.Phone,
+                              Address = cus.Address,
+                          })
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
